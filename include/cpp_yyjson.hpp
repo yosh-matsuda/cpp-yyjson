506d59a8627ae58de92d9934113245ba83872a4c
Yoshiki Matsuda <y_matsuda@fixstars.com>
2024-08-03 14:32:22 +0900

    🔖 Release v0.6.0

--- include/cpp_yyjson.hpp
+++ include/cpp_yyjson.hpp
@@ -1,5 +1,5 @@
 /*===================================================*
-|  cpp-yyjson version v0.5.0                         |
+|  cpp-yyjson version v0.6.0                         |
 |  https://github.com/yosh-matsuda/cpp-yyjson        |
 |                                                    |
 |  Copyright (c) 2024 Yoshiki Matsuda @yosh-matsuda  |
